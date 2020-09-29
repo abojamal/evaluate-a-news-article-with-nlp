fd2ef08a438ff5ea27f1988c840d3313fc63e3c8
shafik <shafiksharif@gmail.com>
2020-09-29 12:25:52 +0300

    4

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,11 +6,35 @@ function handleSubmit(event) {
   Client.checkForName(formText);
 
   console.log('::: Form Submitted :::');
-  fetch('http://localhost:8081/test')
-    .then((res) => res.json())
-    .then(function (res) {
-      document.getElementById('results').innerHTML = res.message;
-    });
+  // console.log(formText);
+  // fetch('http://localhost:8081/test')
+  //   .then((res) => res.json())
+  //   .then(function (res) {
+  //     document.getElementById('results').innerHTML = res.message;
+  //   });
+
+  postData('/api', { txt: formText }).then(function (res) {
+    document.getElementById('results').innerHTML =
+      res.agreement + '<br />' + res.subjectivity;
+  });
 }
 
+// Async POST
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
 export { handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 var path = require('path');
 const express = require('express');
-const mockAPIResponse = require('./mockAPI.js');
-
+//const mockAPIResponse = require('./mockAPI.js');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static('dist'));
 
@@ -18,6 +23,23 @@ app.listen(8081, function () {
   console.log('Example app listening on port 8081!');
 });
 
-app.get('/test', function (req, res) {
-  res.send(mockAPIResponse);
+// app.get('/test', function (req, res) {
+//   res.send(mockAPIResponse);
+// });
+
+const API_KEY = process.env.API_KEY;
+const fetch = require('node-fetch');
+const classification = {};
+app.post('/api', function (req, res) {
+  let text = req.body.txt;
+  console.log(text);
+  fetch(
+    `https://api.meaningcloud.com/sentiment-2.1?key=${API_KEY}&lang=en&txt=${text}&model=general`
+  )
+    .then((data) => data.json())
+    .then(function (data) {
+      classification.agreement = data.agreement;
+      classification.subjectivity = data.subjectivity;
+      res.send(classification);
+    });
 });
