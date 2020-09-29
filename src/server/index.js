ec78b679b40e64087d4ff976c13c5ec0c7d5bbca
shafik <shafiksharif@gmail.com>
2020-09-29 13:38:10 +0300

    7

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,48 @@
-var path = require('path');
+// for working with file and directory paths
+const path = require('path');
+// Require Express to run server and routes
 const express = require('express');
+//To use fetch API
+const fetch = require('node-fetch');
+//To allow the use of environment variables
 const dotenv = require('dotenv');
 dotenv.config();
+// Start up an instance of app
 const app = express();
-//Here we are configuring express to use body-parser as middle-ware.
+//configuring express to use body-parser as middle-ware
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+// Initialize the main project folder
 app.use(express.static('dist'));
-
+// get data route
 app.get('/', function (req, res) {
   res.sendFile('dist/index.html');
-  // res.sendFile(path.resolve('src/client/views/index.html'))
 });
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
   console.log(' listening on port 8081!');
 });
-
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+//refering API_KEY to the environment variable
 const API_KEY = process.env.API_KEY;
-const fetch = require('node-fetch');
-const classification = {};
+// POST route
 app.post('/api', function (req, res) {
+  //Acquiring user submitted text
   let text = req.body.txt;
+  //Sending API request to meaningcloud with key and user acquired text
   fetch(
     `https://api.meaningcloud.com/sentiment-2.1?key=${API_KEY}&lang=en&txt=${text}&model=general`
   )
+    //return the body as promise with json content
     .then((data) => data.json())
-    .then(function (data) {
-      classification.agreement = data.agreement;
-      classification.subjectivity = data.subjectivity;
-      res.send(classification);
+    //storing the needed API response info into the endpoint
+    .then((data) => {
+      projectData.agreement = data.agreement;
+      projectData.subjectivity = data.subjectivity;
+      //send the stored data in endpoint back to client
+      res.send(projectData);
     });
 });
