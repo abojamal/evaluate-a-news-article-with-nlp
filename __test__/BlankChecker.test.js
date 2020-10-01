e142e0d3f2f23d39e7d6c8fccbb92e4ca7b419d7
shafik <shafiksharif@gmail.com>
2020-10-01 20:10:22 +0300

    10

---
+++ __test__/BlankChecker.test.js
@@ -0,0 +1,8 @@
+import { checkForBlank } from '../src/client/js/BlankChecker';
+
+describe('Testing the submit functionality', () => {
+  test('Testing the handleSubmit() function', () => {
+    1;
+    expect(checkForBlank).toBeDefined();
+  });
+});
